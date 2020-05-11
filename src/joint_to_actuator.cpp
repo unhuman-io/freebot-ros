1a363e100feeb6a82dd5a4b83e8eff1a1273ce35
Lee Magnusson <lee@unhuman.io>
2020-05-11 15:32:29 -0700

    joint_to_actuator yaml

--- src/joint_to_actuator.cpp
+++ src/joint_to_actuator.cpp
@@ -13,10 +13,19 @@ class JointToActuator {
   ros::Publisher actuator_pub_;
   ros::Subscriber joint_sub_;
   std::vector<std::string> joint_names_ = {"tool_rotate_joint", "proximal_wrist_joint", "distal_wrist_joint", "jaw_a_joint"};
+  std::vector<std::string> actuator_names_ = {"m1", "m2", "m3", "m4"};
+  Eigen::MatrixXd actuator_transform_ = {4,4};
 };
 
 
 JointToActuator::JointToActuator() {
+  nh_.getParam("joint_names", joint_names_);
+  nh_.getParam("actuator_names", actuator_names_);
+  actuator_transform_.setIdentity();
+  std::vector<double> actuator_transform;
+  if (nh_.getParam("joint_actuator_position_transform", actuator_transform)) {
+      actuator_transform_ = Eigen::Map<Eigen::MatrixXd>(actuator_transform.data(), joint_names_.size(), actuator_names_.size()).transpose();
+  }
   actuator_pub_ = nh_.advertise<sensor_msgs::JointState>("actuator_states", 1, true);
   joint_sub_ = nh_.subscribe<sensor_msgs::JointState>("joint_states", 10, &JointToActuator::callback, this);
 }
@@ -33,12 +42,11 @@ void JointToActuator::callback(const sensor_msgs::JointState::ConstPtr& joint_st
 //      std::cout << it - joint_state->name.begin() << std::endl;
       joint_position(i) = joint_state->position[it - joint_state->name.begin()];
   }
-  Eigen::MatrixXd actuator_transform(4,4);
-  actuator_transform.setIdentity();
-  Eigen::VectorXd actuator_position = actuator_transform * joint_position;
+  
+  Eigen::VectorXd actuator_position = actuator_transform_ * joint_position;
   std::vector<double> actuator_position_vector(actuator_position.data(), actuator_position.data() + actuator_position.size());
   actuator_state.position = actuator_position_vector;
-  actuator_state.name = {"m1", "m2", "m3", "m4"};
+  actuator_state.name = actuator_names_;
   actuator_state.header.stamp = joint_state->header.stamp;
   actuator_pub_.publish(actuator_state);
 }
